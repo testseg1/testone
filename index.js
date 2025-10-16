4ff3eb0d4c3240a006c1e7e5de8d23ddabcbf651
juan.berzal <juan.berzal@bbva.com>
2025-10-16 10:41:58 +0200

    asd

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { exec } = require('child_process');
+const app = express();
+const port = 3000;
+
+app.get('/files', (req, res) => {
+  const filename = req.query.filename; // User-provided input
+
+  if (!filename) {
+    return res.status(400).send('Filename is required.');
+  }
+  
+  // 🚨 VULNERABLE CODE: Direct use of user input in a shell command
+  const command = `ls -l public/${filename}`;
+
+  exec(command, (err, stdout, stderr) => {
+    if (err) {
+      console.error(`Error: ${err.message}`);
+      return res.status(500).send('Internal Server Error');
+    }
+    if (stderr) {
+      console.error(`Stderr: ${stderr}`);
+      return res.status(500).send('Internal Server Error');
+    }
+    res.send(`<pre>${stdout}</pre>`);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
